01f03b5fd0240b785c9cddf7d676af2a858ce026
elias oliveira <eliascpr123@gmail.com>
2022-05-09 07:56:54 -0300

    modified

--- src/components/widgetForm/index.tsx
+++ src/components/widgetForm/index.tsx
@@ -61,7 +61,7 @@ export function WidgetForm() {
         </>
       )}
       <footer className="text-xss text-neutral-400">
-        Feito com S2 pela{" "}
+        Feito com amor pela{" "}
         <a
           className="underline underline-offset-2"
           href="https://rocketseat.com.br"
